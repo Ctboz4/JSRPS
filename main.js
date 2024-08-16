3249390d911de8b312d6c2310152654ff39bf1f3
Ctboz4 <Clayton.T.Boswell@outlook.com>
2024-08-16 12:41:06 -0500

    commit
    I got no idea whats going on

--- main.js
+++ main.js
@@ -2,13 +2,46 @@ const rockButton = document.getElementById("rockButton");
 const paperButton = document.getElementById("paperButton");
 const scissorsButton = document.getElementById("scissorsButton");
 
-rockButton.addEventListener("click", () => playGame("rock"));
-paperButton.addEventListener("click", () => playGame("paper"));
-scissorsButton.addEventListener("click", () => playGame("scissors"));
+
+rockButton.addEventListener("click", () => gamePlay("rock"));
+paperButton.addEventListener("click", () => gamePlay("paper"));
+scissorsButton.addEventListener("click", () => gamePlay("scissors"));
 
 
 function compChoice() {
     const choices = ["rock", "paper", "scissors"];
-    const random = Math.floor(Math.random() * 3);
+    const random = Math.floor(Math.random() * choices.length);
     return choices[random];
+}
+
+function playerChoice(){
+    const choices = ["rock", "paper", "scissors"];
+    return choices;
+}
+
+function gamePlay(){
+    let result;
+    if (compChoice() === playerChoice()){
+        return "It's a tie!";
+    }
+
+    if (compChoice() === "rock" && playerChoice() === "scissors"){
+        return "Computer wins!";
+    }
+
+    if (compChoice() === "rock" && playerChoice() === "paper"){
+        return "Player wins!";
+    }
+
+    if (compChoice() === "paper" && playerChoice() === "rock"){
+        return "Computer wins!";
+    }
+
+    if (compChoice() === "paper" && playerChoice() === "scissors"){
+        return "Player wins!";
+    }
+
+
+    const gameDecision = document.getElementById("gameDecision");
+    gameDecision.textContent = result;
 }
