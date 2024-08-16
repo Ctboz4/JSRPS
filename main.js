63b9b472cf152fcb036539cc680619a1715107ac
Ctboz4 <Clayton.T.Boswell@outlook.com>
2024-08-16 18:22:49 -0500

    commit

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ const paperButton = document.getElementById("paperButton");
 const scissorsButton = document.getElementById("scissorsButton");
 
 
+
+const playerLives = document.getElementById("playerLives");
+
+
+
+const computerLives = document.getElementById("computerLives");
+
+
+
+
 rockButton.addEventListener("click", () => gamePlay("rock"));
 paperButton.addEventListener("click", () => gamePlay("paper"));
 scissorsButton.addEventListener("click", () => gamePlay("scissors"));
@@ -14,34 +24,32 @@ function compChoice() {
     return choices[random];
 }
 
-function playerChoice(){
-    const choices = ["rock", "paper", "scissors"];
-    return choices;
-}
 
-function gamePlay(){
+let computerScore = 0;
+let playerScore = 0;
+function gamePlay(playerChoice){
+    
+    const computerChoice = compChoice();
     let result;
-    if (compChoice() === playerChoice()){
-        return "It's a tie!";
-    }
 
-    if (compChoice() === "rock" && playerChoice() === "scissors"){
-        return "Computer wins!";
+    if (computerChoice === playerChoice) {
+        result = "It's a tie!";
+    } else if (
+        (computerChoice === "rock" && playerChoice === "scissors") ||
+        (computerChoice === "scissors" && playerChoice === "paper") ||
+        (computerChoice === "paper" && playerChoice === "rock")
+    ) {
+        result = "Computer wins!"; computerScore++;
+    } else {
+        result = "Player wins!" ; playerScore++;
     }
-
-    if (compChoice() === "rock" && playerChoice() === "paper"){
-        return "Player wins!";
-    }
-
-    if (compChoice() === "paper" && playerChoice() === "rock"){
-        return "Computer wins!";
-    }
-
-    if (compChoice() === "paper" && playerChoice() === "scissors"){
-        return "Player wins!";
-    }
-
 
     const gameDecision = document.getElementById("gameDecision");
-    gameDecision.textContent = result;
+    gameDecision.textContent = `Player chose ${playerChoice}, Computer chose ${computerChoice}. ${result}`;
+
+    const computerScoreElement = document.getElementById("computerScore");
+    const playerScoreElement = document.getElementById("playerScore");
+    computerScoreElement.textContent = "Computer Score" + " " + computerScore;
+    playerScoreElement.textContent = "Player score" + " " + playerScore;
+    
 }
